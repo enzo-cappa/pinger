c13233d8760045b37922347944d50465251a875f
Enzo Cappa <zcappa@gmail.com>
2015-09-21 18:00:40 -0300

    more work

--- pinger.rb
+++ pinger.rb
@@ -1,5 +1,16 @@
 #!/usr/bin/env ruby
 require 'net/ping'
+require 'pg'
+
+conn = PGconn.connect("ip adddress", 5432, '', '', "db name", "user", "password")
+conn.prepare('statement1', "INSERT INTO games (game_hash,team1,team2,team1score,team2score,time,update_at) VALUES ($1,$2,$3,$4,$5,$6,$7)");
+def save_result  
+  conn.exec_prepared('statement1', [myId, team1, team1score, team2, team2score, progress, update]);
+  res  = conn.exec('select tablename, tableowner from pg_tables')
+  res.each do |row|
+    puts row['tablename'] + ' | ' + row['tableowner']
+  end
+end
 
 @icmp = Net::Ping::ICMP.new('google.com')
 rtary = []
