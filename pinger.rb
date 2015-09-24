17feac3b93166cb5548eed7e00dee1d8fbc06fee
Enzo Cappa <zcappa@gmail.com>
2015-09-24 16:51:36 -0300

    fixed

--- pinger.rb
+++ pinger.rb
@@ -1,32 +1,25 @@
 #!/usr/bin/env ruby
 require 'net/ping'
-require 'pg'
+require 'sqlite3'
 
-conn = PGconn.connect("ip adddress", 5432, '', '', "db name", "user", "password")
-conn.prepare('statement1', "INSERT INTO games (game_hash,team1,team2,team1score,team2score,time,update_at) VALUES ($1,$2,$3,$4,$5,$6,$7)");
-def save_result  
-  conn.exec_prepared('statement1', [myId, team1, team1score, team2, team2score, progress, update]);
-  res  = conn.exec('select tablename, tableowner from pg_tables')
-  res.each do |row|
-    puts row['tablename'] + ' | ' + row['tableowner']
-  end
+@db = SQLite3::Database.new 'pinger'
+@db.execute "CREATE TABLE IF NOT EXISTS ping(ping_date INTEGER PRIMARY KEY, ping_result INTEGER, ping_duration INTEGER)"
+
+def save_result(ping_date, ping_result, ping_duration)
+  @db.execute("INSERT INTO ping(ping_date, ping_result, ping_duration) VALUES (?,?,?)", ping_date, ping_result, ping_duration )
 end
 
 @icmp = Net::Ping::ICMP.new('google.com')
-rtary = []
-pingfails = 0
-repeat = 5
 puts 'starting to ping'
 while true do
-	if @icmp.ping
-		rtary << @icmp.duration
-		puts "host replied in #{@icmp.duration}"
-	else
-		pingfails += 1
-		puts "timeout"
-	end
-	sleep 5
+  ping_date = Time.now.getutc.to_i
+  ping_result = 0
+  ping_duration = -1
+  if @icmp.ping
+    ping_duration = @icmp.duration
+    ping_result = 1
+    puts "host replied in #{@icmp.duration}"
+  end  
+  save_result(ping_date, ping_result, ping_duration)
+  sleep 5
 end
-avg = rtary.inject(0) {|sum, i| sum + i}/(repeat - pingfails)
-puts "Average round-trip is #{avg}\n"
-puts "#{pingfails} packets were droped"
